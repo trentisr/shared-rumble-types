977c4119b5f0afff5baf2d77356fb1cb88ac6f6b
Trent Breneman <tbreneman@icloud.com>
2025-06-30 14:27:54 -0600

    update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,8 @@
+export interface Answer {
+    id: string;
+    text?: string;
+    isCorrect: boolean;
+}
 export interface Question {
     id: string;
     text: string;
@@ -5,9 +10,48 @@ export interface Question {
     image?: string;
     options?: Answer[];
     answered?: boolean;
+    textInputCorrectAnswer?: string | number;
 }
-export interface Answer {
+export interface TeamMember {
     id: string;
-    text?: string;
-    isCorrect: boolean;
+    name: string;
 }
+export interface Team {
+    id?: string;
+    name: string;
+    type: 'Single' | 'Team';
+    members: TeamMember[];
+    captain?: string;
+}
+export interface Schematics {
+    timeLimit: number;
+    timeGap: number;
+    numRounds: number;
+    gameMode: 'manual' | 'automatic';
+    startTime?: Date | string;
+}
+export interface DetailedPlayer {
+    id: string;
+    name: string;
+}
+export interface Rumble {
+    id: string;
+    name: string;
+    status: 'active' | 'completed' | 'draft';
+    questions: Question[];
+    teams: Team[];
+    schematics: Schematics;
+    playerDetails?: DetailedPlayer[];
+}
+export interface RumbleCreateInput {
+    name: string;
+    description?: string;
+    created_by?: string;
+    questions?: Question[];
+    schematics?: Schematics;
+    teams?: Team[];
+    status?: 'active' | 'completed' | 'draft';
+}
+export type RumbleUpdateInput = Partial<RumbleCreateInput> & {
+    id?: string;
+};
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,2 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// Keep any other shared interfaces you have in this file
