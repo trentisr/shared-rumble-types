db173953a8f4a9577affeee8570d8b6827fd24d3
Trent Breneman <tbreneman@icloud.com>
2025-07-01 21:12:45 -0600

    type updates

--- index.ts
+++ index.ts
@@ -1,17 +1,31 @@
 export interface Answer {
-  id: string;
-  text?: string;
+  readonly id: string;
+  text: string;
   isCorrect: boolean;
 }
 
+export interface PlayerRecordedAnswer extends Answer {
+  answer: string;
+  isCorrect: boolean;
+  score: number;
+  timeTaken: number;
+}
+
 export interface Question {
-  id: string;
+  readonly id: string;
   text: string;
-  type: 'multiple_choice' | 'textinput' | 'image_multiple_choice' | 'image_textinput' | 'truefalse' | 'mcq' | 'image_truefalse' | 'image_mcq';
+  type:
+    | 'textinput'
+    | 'truefalse'
+    | 'mcq'
+    | 'image_textinput'
+    | 'image_truefalse'
+    | 'image_mcq';
   image?: string;
+  timer?: number;
   options?: Answer[];
-  answered?: boolean;
   textInputCorrectAnswer?: string | number;
+  correctAnswer?: string;
 }
 
 export interface TeamMember {
@@ -19,11 +33,21 @@ export interface TeamMember {
   name: string;
 }
 
-export interface Team {
-  id?: string;
+export interface DetailedPlayer {
+  readonly id: string;
   name: string;
+  score: number;
+  answers: { [questionId: string]: PlayerRecordedAnswer };
   type: 'Single' | 'Team';
-  members: TeamMember[];
+  teamName?: string;
+}
+
+export interface Team {
+  readonly id?: string;
+  name: string;
+  players?: string[];
+  type: 'Single' | 'Team';
+  members: DetailedPlayer[];
   captain?: string;
 }
 
@@ -35,33 +59,32 @@ export interface Schematics {
   startTime?: Date | string;
 }
 
-export interface DetailedPlayer {
-  id: string;
-  name: string;
-}
-
 export interface Rumble {
-  id: string;
+  readonly id: string;
   name: string;
-  description: string | null;
-  create_time: Date;
-  created_by: string | null;
-  deleted: Date | null;
   status: 'active' | 'completed' | 'draft' | 'in-game';
   questions: Question[];
   teams: Team[];
   schematics: Schematics;
   playerDetails?: DetailedPlayer[];
+  currentQuestionIndex: number;
+  gameMode: 'manual' | 'automatic';
+  description: string | null;
+  create_time: Date;
+  created_by: string | null;
+  deleted: Date | null;
 }
 
 export interface RumbleCreateInput {
   name: string;
   description?: string | null;
-  created_by?: string | null;
+  created_by?: string;
   questions?: Question[];
   schematics?: Schematics;
   teams?: Team[];
-  status?: 'active' | 'completed' | 'draft' | 'in-game';
+  status?: 'active' | 'completed' | 'draft';
 }
 
-export type RumbleUpdateInput = Partial<Rumble>;
+export type RumbleUpdateInput = Partial<RumbleCreateInput> & {
+  readonly id?: string;
+};
